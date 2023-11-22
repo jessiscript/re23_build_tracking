e52430e390136cba475500989de4079d728f81a8
Chris Fleger <cb.fleger@live.de>
2023-11-22 09:50:38 +0100

    Improved RemoteBuildTracking.py

--- RemoteBuildTracking.py
+++ RemoteBuildTracking.py
@@ -1,15 +1,18 @@
 import json
 import urlfetch
 import base64
-import os
+import re
+import pytz
 import argparse
 import pandas as pd
 from dateutil import parser
+from matplotlib.ticker import FuncFormatter
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Retrieve native image size from last GitHub commit using setup-graalvm action.")
+    parser.add_argument("owner", help="Username of the GitHub repository owner")
     parser.add_argument("repo_path", help="Path to your GitHub repository")
     parser.add_argument("branch", help="Name of the branch")
     parser.add_argument("n", help="Last n commits")
@@ -34,19 +37,19 @@ def get_response(url, token):
     return None
 
 def get_image_size(commit_sha):
-    data = get_response('https://api.github.com/repos/jessiscript/' + repo_path + '/git/ref/metrics/' + commit_sha, token)
+    data = get_response('https://api.github.com/repos/' + owner + '/' + repo_path + '/git/ref/graalvm-metrics/' + commit_sha, token)
     if data != None:
         ref_sha = data.get("object").get("sha")
     else:
         return 0
 
-    data = get_response('https://api.github.com/repos/jessiscript/' + repo_path + '/git/trees/' + ref_sha, token)
+    data = get_response('https://api.github.com/repos/' + owner + '/' + repo_path + '/git/trees/' + ref_sha, token)
     if data != None:
         blob_sha = data.get("tree")[0].get("sha")
     else:
         return 0
 
-    data = get_response('https://api.github.com/repos/jessiscript/' + repo_path + '/git/blobs/' + blob_sha, token)
+    data = get_response('https://api.github.com/repos/' + owner + '/' + repo_path + '/git/blobs/' + blob_sha, token)
     if data != None:
         content = base64.b64decode(data.get("content"))
         data = json.loads(content)
@@ -55,52 +58,105 @@ def get_image_size(commit_sha):
         return 0
     
 def format_date(date):
-    return parser.isoparse(date).strftime('%d.%m.%Y \n %H:%M')
+    # Parse the timestamp and convert it to the desired timezone
+    commit_time = parser.isoparse(date)
+    commit_time_utc = commit_time.replace(tzinfo=pytz.utc)
+    desired_timezone = pytz.timezone('Europe/Berlin')
+    commit_time_local = commit_time_utc.astimezone(desired_timezone)
+    return commit_time_local.strftime('%d.%m.%Y \n %H:%M')
 
 if __name__ == "__main__":
     args = parse_args()
     
+    owner = args.owner
     repo_path = args.repo_path
     branch = args.branch
     n = args.n
     token = args.token
 
+    if not n.isnumeric():
+        print("Wrong use: n should be a number")
+        exit(1)
+    else:
+        n = int(n)
+
     try:  
-        response = urlfetch.get('https://api.github.com/repos/jessiscript/' + repo_path + '/commits', params = {'per_page': n})
-        data = json.loads(response.content)
-        print(data)
-        
-        shas = [commit.get('sha') for commit in data]
-        
-        #print("The image file has a total size of " + str(data.get("image_details").get("total_bytes")) + " bytes.")
+
+        response = urlfetch.get('https://api.github.com/repos/jessiscript/' + repo_path + '/events', headers={
+            "Authorization": "Bearer " + token
+        })
+        link_header = response.headers.get("link")
+        response_json = json.loads(response.content)
+        commits_left = n
+        push_events = []
+
+        for event in response_json:
+            if commits_left <= 0:
+                break
+            if event["type"] == "PushEvent" and event["payload"]["ref"] == ("refs/heads/" + branch):
+                push_events.append(event)
+                commits_left = commits_left-1
+
+        # Check for pagination
+            link_header = response.headers.get("link")
+            next_page_match = re.search(r'<([^>]+)>;\s*rel="next"', link_header)
+
+        while link_header and "rel=\"next\"" in link_header and commits_left > 0:
+            # Extract the URL for the next page
+            next_page_url = next_page_match.group(1)
+
+            # Make the request for the next page
+            response = urlfetch.fetch(next_page_url, headers={
+                "Authorization": "Bearer " + token
+            })
+            response_json = json.loads(response.content)
+
+            for event in response_json:
+                if commits_left <= 0:
+                    break
+                if event["type"] == "PushEvent" and event["payload"]["ref"] == ("refs/heads/" + branch):
+                    push_events.append(event)
+                    commits_left = commits_left-1
+
+            # Update the next_page_match for the next iteration
+            link_header = response.headers.get("link")
+            next_page_match = re.search(r'<([^>]+)>;\s*rel="next"', link_header)
+
+        # Prepare data
+        timestamps = []
+        shas = []
+        for push_event in push_events:
+            timestamps.append(push_event.get("created_at"))
+            shas.append(push_event.get("payload").get("commits")[-1].get("sha"))
 
         # Extract data for plotting
-        commit_dates = [format_date(commit.get('commit').get('author').get('date')) for commit in data]
-        image_sizes = [get_image_size(sha) for sha in shas]
-
+        commit_dates = [format_date(timestamp) for timestamp in timestamps]
+        image_sizes = [get_image_size(sha) / 1e6 for sha in shas]
 
         # Create a DataFrame for Seaborn
-        image_data = pd.DataFrame({"Commit Dates": list(reversed(commit_dates)), "Image Size in MB": list(reversed(image_sizes))})
-        #print(image_data)
+        image_data = pd.DataFrame({"Commit Dates": list(reversed(commit_dates)), "Image Size (MB)": list(reversed(image_sizes))})
+
+        # Formatting Y-axis tick labels to display in MB
+        def format_mb(x, _):
+            return f"{x:.0f} MB"
+        plt.gca().yaxis.set_major_formatter(FuncFormatter(format_mb))
 
         # Set the size of the figure
         plt.figure(figsize=(15, 11))  
 
-        # Create a Seaborn point plot
-        sns.set_theme(style="darkgrid")
-        sns.pointplot(x="Commit Dates", y="Image Size in MB", data=image_data)
-
         # Rotate x-axis labels for better readability
-        if int(n) > 10 :
+        if n > 10:
             plt.xticks(rotation=45)
 
-        #plot
+        # Create a Seaborn point plot
+        sns.set_theme(style="darkgrid")
+        sns.pointplot(x="Commit Dates", y="Image Size (MB)", data=image_data)
         plt.xlabel("Commit Dates")
         plt.ylabel("Image Size in MB")
         plt.title("Development of Native Image Sizes")
 
         # Save the plot as a .png file
-        plt.savefig("output2_plot.png")
+        plt.savefig("output_plot.png")
 
 
     except Exception as e: 
